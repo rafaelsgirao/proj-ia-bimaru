82fa5eb291b06f0220dd51c3128caa82b2380814
Rafael Girão <git@rafael.ovh>
2023-05-25 16:54:23 +0100

    :sparkles: :poop: Continue implementing stuff (not 100% done)

--- src/bimaru.py
+++ src/bimaru.py
@@ -88,7 +88,7 @@ class Board:
                     elif left == 1 and right == 1:
                         rprint("m")
                     elif left == 0 and right == 1:
-                        rprint("l")  # TODO ocntinuar
+                        rprint("l")
                     elif top == 1 and bottom == 0:
                         rprint("b")
                     elif top == 1 and bottom == 1:
@@ -106,9 +106,19 @@ class Board:
         board = Board()
 
         board.remaining_ships = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1]
+        board.used_ships = []  # A list of tuples, e.g [(ship_size, index)]
         board.positions = np.full((10, 10), 0)
 
         return board
+
+    def copy(self):
+        """Retorna uma instância de Board idêntica à atual."""
+        new_board = Board()
+        new_board.remaining_ships = self.remaining_ships.copy()
+        new_board.positions = self.positions.copy()
+        new_board.used_ships = board.used_ships.copy()
+
+        return new_board
 
 
 class Bimaru(Problem):
@@ -175,23 +185,6 @@ class Bimaru(Problem):
         # Remove padding
         self.hints = self.hints[1:-1, 1:-1]
 
-    def is_valid_board(self, board):
-        for row in range(len(self.rows)):
-            board_row_sum = np.sum(board.positions[row, :])
-            # debug(
-            #    f"board_row_sum ({row}) = {board_row_sum} | row_val = {self.rows[row]}"
-            # )
-            if board_row_sum > self.rows[row]:
-                return False
-        for col in range(len(self.cols)):
-            board_col_sum = np.sum(board.positions[:, col])
-            # debug(
-            #    f"board_col_sum ({col}) = {board_col_sum} | col_val = {self.cols[row]}"
-            # )
-            if board_col_sum > self.cols[col]:
-                return False
-        return True
-
     def has_conflicts_with_hints(self, matrix):
         debug(f"\nMATRIZ DE INPUT: \n {matrix}")
         debug(f"\nMATRIZ DAS HINTS: \n {self.hints}")
@@ -206,7 +199,7 @@ class Bimaru(Problem):
         m = []
 
         for (row, col), _ in np.ndenumerate(np.zeros((10, 10 - n + 1))):
-            new_matrix = np.zeros((10, 10))
+            new_matrix = np.zeros((10, 10), dtype=int)
             for i in range(n):
                 new_matrix[row, col + i] = 1
                 # if self.problem.insertion_conflicts_with_hints(row, col + i):
@@ -236,22 +229,27 @@ class Bimaru(Problem):
         """Retorna uma lista de ações que podem ser executadas a
         partir do estado passado como argumento."""
 
-        rem_ships = state.board.remaining_ships
+        rem_ships = state.board.remaining_ships.copy()
         if len(rem_ships) == 0:
-            return []
+            yield (None, None)
 
-        # Get the next largest ship that hasn't been placed.
-        while True:
-            next_ship = rem_ships.pop(0)
-            for m in self.possible_matrices[next_ship]:
-                pass  # Fuck. Esqueci me de implementar aqui a funcao do nunmpy_things.py
+            # Get the next largest ship that hasn't been placed.
+            # while True:  # Look breaks when rem_ships.pop fails.
+        next_ship = rem_ships.pop(0)
+        debug(f"next_ship = {next_ship}")
+        for matrix_i in range(len(self.possible_matrices[next_ship])):
+            m = self.possible_matrices[next_ship][matrix_i]
+            if not self.matrices_in_conflict(m, state.board.positions):
+                yield (next_ship, matrix_i)
+        raise ValueError  # FIXME: retornar tuplo vazio p/ nao haver acoes
 
     """
-    Verifica se ambas as matrizes estão em conflito ou se podem coexistir.
+    Verifica se dois boards estão em conflito ou se a sua junção pode existir. (TODO: verificar board resultante c/ is_valid_board)
     """
 
-    def boards_in_conflict(b1: Board, b2: Board) -> bool:
-        m1, m2 = b1.positions, b2.positions
+    @staticmethod
+    def matrices_in_conflict(m1, m2) -> bool:
+        #    m1, m2 = b1.positions, b2.positions
         m_sums = np.empty_like(m1)
         m2_pad = np.pad(m2, pad_width=1, mode="constant", constant_values=0)
         #  print(m2_pad)
@@ -259,9 +257,9 @@ class Bimaru(Problem):
         for (i, j), _ in np.ndenumerate(m_sums):
             # m1_around_point = m1[i: i+ 2, j: j+2]
             m2_around_point = m2_pad[i : i + 3, j : j + 3]  # 3x3 matrix around point
-            print(f"({i}, {j})")
-            print(m2_around_point)
-            print("---")
+            # debug(f"({i}, {j})")
+            # debug(m2_around_point)
+            # debug("---")
             m_sums[i, j] = np.sum(m2_around_point * m_filter) * m1[i, j] + m1[i, j]
 
         return np.any(m_sums > 1)
@@ -271,8 +269,41 @@ class Bimaru(Problem):
         'state' passado como argumento. A ação a executar deve ser uma
         das presentes na lista obtida pela execução de
         self.actions(state)."""
-        # TODO
-        pass
+
+        debug(f"action = {action}")
+        new_board = state.board.copy()
+        ship_matrix = self.possible_matrices[action[0]][action[1]]
+        new_board.positions += ship_matrix[1]
+        # debug(new_board.remaining_ships)
+        new_board.remaining_ships.remove(action[0])
+        new_board.used_ships.append(action)
+
+        return BimaruState(new_board)
+
+    def is_valid_board(self, board, goal_test=False):
+        def cmp(a, b, goal_test):
+            if goal_test:
+                return a == b
+            else:
+                return a > b
+
+        for row in range(len(self.rows)):
+            board_row_sum = np.sum(board.positions[row, :])
+            # debug(
+            #    f"board_row_sum ({row}) = {board_row_sum} | row_val = {self.rows[row]}"
+            # )
+            #    if board_row_sum > self.rows[row]:
+            if cmp(board_row_sum, self.rows[row]):
+                return False
+        for col in range(len(self.cols)):
+            board_col_sum = np.sum(board.positions[:, col])
+            # debug(
+            #    f"board_col_sum ({col}) = {board_col_sum} | col_val = {self.cols[row]}"
+            # )
+            #  if board_col_sum > self.cols[col]:
+            if cmp(board_col_sum, self.cols[col]):
+                return False
+        return True
 
     def goal_test(self, state: BimaruState):
         """Retorna True se e só se o estado passado como argumento é
@@ -281,7 +312,8 @@ class Bimaru(Problem):
 
         # Only check if all rows and columns are filled:
         # More profound checks should be done before/when filling positions.
-        return np.all(state.problem.cols == 0) and np.all(state.problem.rows == 0)
+        # return np.all(state.problem.cols == 0) and np.all(state.problem.rows == 0)
+        return self.is_valid_board(state.board, goal_test=True)
 
     def h(self, node: Node):
         """Função heuristica utilizada para a procura A*."""
@@ -313,7 +345,7 @@ if __name__ == "__main__":
     problem = Bimaru(board)
     board.print()
     # print(problem.gen_matrices(1))
-    # initial_state = BimaruState(board)
+    s1 = BimaruState(board)
 
     # Usar uma técnica de procura para resolver a instância,
     # Retirar a solução a partir do nó resultante,
